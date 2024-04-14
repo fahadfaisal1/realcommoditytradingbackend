93de5f7f90d6137e9befea79f08024ae02ba2532
AbdulMoiz36 <abdulmoiz8236@gmail.com>
2024-04-14 23:15:27 +0500

    verified offers

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
+const VerifiedOffer = require("./routes/verified_offers");
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Middlewares
 const app = express();
+
+// Set up CORS
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+
+// Use JSON middleware
 app.use(express.json());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://moiz36:4RGR6pM_Yh-cx7z@cluster0.ocumynd.mongodb.net/realcommoditytrading')
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 // Routes
 app.use("/home", home);
+app.use("/verified_offers", VerifiedOffer);
 
-// connection
+// Start the server
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
---
+++ routes/verified_offers.js
@@ -0,0 +1,42 @@
+
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+
+const verified_offers_Mongoose = new Schema({
+    "_id": mongoose.ObjectId,
+    "id": Number,
+    "offer_title": String,
+    "offer_description": String,
+    "subcat_id": Number,
+    "offer_type": String,
+    "login_session_id": String,
+    "visitors_count": String,
+    "post_visitors_count": Number,
+    "v_offer_files": String,
+    "announcement_show_on": String,
+    "hide_show_status": Number,
+    "created_at": Date,
+    "updated_at": String,
+}, { collection: "verified_offers" })
+
+const VerifiedOffer = mongoose.model('VerifiedOffer', verified_offers_Mongoose);
+
+
+router.get('/', async (req, res) => {
+    try {
+      // Use Mongoose to find all documents in the "verified_offers" collection
+      const offers = await VerifiedOffer.find();
+  
+      // Return the fetched data as a response
+      res.json(offers);
+    } catch (error) {
+      // If an error occurs, return an error response
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+module.exports = router;
